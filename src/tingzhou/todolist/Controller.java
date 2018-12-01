2d64e9ea5c4178b82087ad8d35c8c4a8075a5456
Ting Zhou <limtingzhou@gmail.com>
2018-12-01 20:42:02 +0800

    Add ListView and TextArea to BorderPane
    Uses a BorderPane as the format for the application.
    Places ListView on the left side to view all to-do items.
    Places TextArea in the center to view details of selected to-do item.

--- src/tingzhou/todolist/Controller.java
+++ src/tingzhou/todolist/Controller.java
@@ -1,4 +1,61 @@
 package tingzhou.todolist;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextArea;
+import tingzhou.todolist.datamodel.TodoItem;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Controller {
+    private List<TodoItem> todoItems;
+
+    @FXML
+    private ListView<TodoItem> todoListView;
+    @FXML
+    private TextArea itemDetailsTextArea;
+
+    public void initialize() {
+        TodoItem item1 = new TodoItem("Mail birthday card", "buy a birthday card for John",
+                LocalDate.of(2016, Month.APRIL,25));
+
+        TodoItem item2 = new TodoItem("Doctor's appointment", "See Dr. Smith at 123 Main Street. Bring paperwork",
+                LocalDate.of(2016, Month.MAY,23));
+
+        TodoItem item3 = new TodoItem("Finish design proposal for client", "I promised Mike I'd email website mockups by Friday 22 April",
+                LocalDate.of(2016, Month.APRIL,22));
+
+        TodoItem item4 = new TodoItem("Pickup Doug at the train station", "Doug's arriving on March 23 on the 5:00 train",
+                LocalDate.of(2016, Month.MARCH,23));
+
+        TodoItem item5 = new TodoItem("Pick up dry cleaning", "The clothes should be ready by Wednesday",
+                LocalDate.of(2016, Month.APRIL,20));
+
+        todoItems = new ArrayList<>();
+        todoItems.add(item1);
+        todoItems.add(item2);
+        todoItems.add(item3);
+        todoItems.add(item4);
+        todoItems.add(item5);
+
+        todoListView.getItems().setAll(todoItems);
+        todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+    }
+
+    @FXML
+    public void handleClickListView() {
+        TodoItem item = todoListView.getSelectionModel().getSelectedItem();
+
+        StringBuilder sb = new StringBuilder(item.getDetails());
+        sb.append("\n\n\n\n");
+        sb.append("Due: ");
+        sb.append(item.getDeadline().toString());
+
+        itemDetailsTextArea.setText(sb.toString());
+
+    }
 }
