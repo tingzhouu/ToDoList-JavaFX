06136d50aa8c04e62c49bd782fef170227fbeca1
Ting Zhou <limtingzhou@gmail.com>
2018-12-02 11:21:43 +0800

    Adds Save and Load Functionality
    Previously, we hard-coded items in the to-do list.
    The items are now saved in a .txt file which loads when application starts.
    The items will also be saved in the .txt file when application closes.

--- src/tingzhou/todolist/Controller.java
+++ src/tingzhou/todolist/Controller.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextArea;
+import tingzhou.todolist.datamodel.TodoData;
 import tingzhou.todolist.datamodel.TodoItem;
 
 import java.time.LocalDate;
@@ -27,27 +28,29 @@ public class Controller {
     private Label deadlineLabel;
 
     public void initialize() {
-        TodoItem item1 = new TodoItem("Mail birthday card", "buy a birthday card for John",
-                LocalDate.of(2016, Month.APRIL,25));
-
-        TodoItem item2 = new TodoItem("Doctor's appointment", "See Dr. Smith at 123 Main Street. Bring paperwork",
-                LocalDate.of(2016, Month.MAY,23));
-
-        TodoItem item3 = new TodoItem("Finish design proposal for client", "I promised Mike I'd email website mockups by Friday 22 April",
-                LocalDate.of(2016, Month.APRIL,22));
-
-        TodoItem item4 = new TodoItem("Pickup Doug at the train station", "Doug's arriving on March 23 on the 5:00 train",
-                LocalDate.of(2016, Month.MARCH,23));
-
-        TodoItem item5 = new TodoItem("Pick up dry cleaning", "The clothes should be ready by Wednesday",
-                LocalDate.of(2016, Month.APRIL,20));
-
-        todoItems = new ArrayList<>();
-        todoItems.add(item1);
-        todoItems.add(item2);
-        todoItems.add(item3);
-        todoItems.add(item4);
-        todoItems.add(item5);
+//        TodoItem item1 = new TodoItem("Mail birthday card", "buy a birthday card for John",
+//                LocalDate.of(2016, Month.APRIL,25));
+//
+//        TodoItem item2 = new TodoItem("Doctor's appointment", "See Dr. Smith at 123 Main Street. Bring paperwork",
+//                LocalDate.of(2016, Month.MAY,23));
+//
+//        TodoItem item3 = new TodoItem("Finish design proposal for client", "I promised Mike I'd email website mockups by Friday 22 April",
+//                LocalDate.of(2016, Month.APRIL,22));
+//
+//        TodoItem item4 = new TodoItem("Pickup Doug at the train station", "Doug's arriving on March 23 on the 5:00 train",
+//                LocalDate.of(2016, Month.MARCH,23));
+//
+//        TodoItem item5 = new TodoItem("Pick up dry cleaning", "The clothes should be ready by Wednesday",
+//                LocalDate.of(2016, Month.APRIL,20));
+//
+//        todoItems = new ArrayList<>();
+//        todoItems.add(item1);
+//        todoItems.add(item2);
+//        todoItems.add(item3);
+//        todoItems.add(item4);
+//        todoItems.add(item5);
+//
+//        TodoData.getInstance().setTodoItems(todoItems);
 
         todoListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TodoItem>() {
             @Override
@@ -61,7 +64,7 @@ public class Controller {
             }
         });
 
-        todoListView.getItems().setAll(todoItems);
+        todoListView.getItems().setAll(TodoData.getInstance().getTodoItems());
         todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         todoListView.getSelectionModel().selectFirst();
     }
