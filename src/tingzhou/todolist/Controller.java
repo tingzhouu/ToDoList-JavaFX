b0715584d550d467005eae9501f93aa3fe486b83
Ting Zhou <limtingzhou@gmail.com>
2018-12-02 13:36:33 +0800

    Add function to input items to the list
    The MenuBar was added to allow users to add items to the list
    When "New..." button is clicked, a dialogbox appears to prompt for input.
    The item created will then be selected.
    
    WrapText was added to the dialogbox for easy viewing of long descriptions.

--- src/tingzhou/todolist/Controller.java
+++ src/tingzhou/todolist/Controller.java
@@ -1,21 +1,18 @@
 package tingzhou.todolist;
 
-import com.sun.tools.javac.comp.Todo;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.SelectionMode;
-import javafx.scene.control.TextArea;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
 import tingzhou.todolist.datamodel.TodoData;
 import tingzhou.todolist.datamodel.TodoItem;
 
-import java.time.LocalDate;
-import java.time.Month;
+import java.io.IOException;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class Controller {
     private List<TodoItem> todoItems;
@@ -26,31 +23,10 @@ public class Controller {
     private TextArea itemDetailsTextArea;
     @FXML
     private Label deadlineLabel;
+    @FXML
+    private BorderPane mainBorderPane;
 
     public void initialize() {
-//        TodoItem item1 = new TodoItem("Mail birthday card", "buy a birthday card for John",
-//                LocalDate.of(2016, Month.APRIL,25));
-//
-//        TodoItem item2 = new TodoItem("Doctor's appointment", "See Dr. Smith at 123 Main Street. Bring paperwork",
-//                LocalDate.of(2016, Month.MAY,23));
-//
-//        TodoItem item3 = new TodoItem("Finish design proposal for client", "I promised Mike I'd email website mockups by Friday 22 April",
-//                LocalDate.of(2016, Month.APRIL,22));
-//
-//        TodoItem item4 = new TodoItem("Pickup Doug at the train station", "Doug's arriving on March 23 on the 5:00 train",
-//                LocalDate.of(2016, Month.MARCH,23));
-//
-//        TodoItem item5 = new TodoItem("Pick up dry cleaning", "The clothes should be ready by Wednesday",
-//                LocalDate.of(2016, Month.APRIL,20));
-//
-//        todoItems = new ArrayList<>();
-//        todoItems.add(item1);
-//        todoItems.add(item2);
-//        todoItems.add(item3);
-//        todoItems.add(item4);
-//        todoItems.add(item5);
-//
-//        TodoData.getInstance().setTodoItems(todoItems);
 
         todoListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TodoItem>() {
             @Override
@@ -64,9 +40,42 @@ public class Controller {
             }
         });
 
-        todoListView.getItems().setAll(TodoData.getInstance().getTodoItems());
+        todoListView.setItems(TodoData.getInstance().getTodoItems());
         todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         todoListView.getSelectionModel().selectFirst();
+    }
+
+    @FXML
+    public void showNewItemDialog() {
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.initOwner(mainBorderPane.getScene().getWindow());
+
+        dialog.setTitle("Add new Todo Item");
+        dialog.setHeaderText("Use this dialog to create a new todo item");
+
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("todoItemDialog.fxml"));
+
+        try {
+            dialog.getDialogPane().setContent(fxmlLoader.load());
+        } catch (IOException e) {
+            System.out.println("Couldn't print load the dialog");
+            e.printStackTrace();
+            return;
+        }
+
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+
+        Optional<ButtonType> result = dialog.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+            DialogController controller = fxmlLoader.getController();
+            TodoItem newItem = controller.processResults();
+
+            todoListView.getSelectionModel().select(newItem);
+
+        }
+
     }
 
 
